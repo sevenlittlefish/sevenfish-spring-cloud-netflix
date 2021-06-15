38034848380b9cb3ebc4d02a21dd55a2fca64c23
liuhongru <1073655408@qq.com>
2021-06-15 22:06:06 +0800

    load balance rule

--- sevenfish-spring-cloud-consumer/consumer-demo/src/main/java/com/cloud/config/CloudConfig.java
+++ sevenfish-spring-cloud-consumer/consumer-demo/src/main/java/com/cloud/config/CloudConfig.java
@@ -1,6 +1,8 @@
 package com.cloud.config;
 
 import brave.sampler.Sampler;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +18,17 @@ public class CloudConfig {
     @Bean
     //告诉spring cloud创建一个支持ribbon的RestTemplate类
     @LoadBalanced
-    RestTemplate restTemplate(){
+    public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    /**
+     * 负载均衡策略，IRule实现类
+     * @return
+     */
+    @Bean
+    public IRule rule(){
+        return new RandomRule();
     }
 
     /**
